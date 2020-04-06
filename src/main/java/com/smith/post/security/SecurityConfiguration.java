94f258976d1fd17f95aa70e33feaf7313d9a361b
danielsmith <smithdaniel@live.com>
2020-04-05 20:08:47 -0400

    added security, in memory for now, soon JDBC then oAuth2

---
+++ src/main/java/com/smith/post/security/SecurityConfiguration.java
@@ -0,0 +1,72 @@
+package com.smith.post.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	
+//	@Autowired
+//	DataSource dataSource;
+	
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+//		auth.jdbcAuthentication().dataSource(dataSource)
+//		.withDefaultSchema()
+		
+		
+		auth.inMemoryAuthentication()
+		.withUser("myuser")
+		.password("pass")
+		.roles("USER")
+		.and()
+		.withUser("dan")
+		.password("pass")
+		.roles("USER")
+		.and()
+		.withUser("managerUser")
+		.password("pass")
+		.roles("ADMIN")
+		.and()
+		.withUser("Navatha")
+		.password("pass")
+		.roles("ADMIN")
+		.and()
+		.withUser("Daniel")
+		.password("pass")
+		.roles("ADMIN");
+		
+
+	
+		
+	}
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.authorizeRequests()
+		.antMatchers("/items/new").hasRole("ADMIN")
+		.antMatchers("/kitchens/new").hasRole("ADMIN")
+		.antMatchers("/").authenticated()
+		.and().formLogin();
+	
+		
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
+		http.cors();
+	}
+}
