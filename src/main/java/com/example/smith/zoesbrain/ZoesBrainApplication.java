56c5959caaa60e6ea5c8052b442c5d21dd93da67
Daniel Smith <memorysight@gmail.com>
2024-08-02 19:39:37 -0400

    initial commit

---
+++ src/main/java/com/example/smith/zoesbrain/ZoesBrainApplication.java
@@ -0,0 +1,13 @@
+package com.example.smith.zoesbrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoesBrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZoesBrainApplication.class, args);
+    }
+
+}
